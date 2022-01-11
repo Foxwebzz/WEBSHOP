beff062a4e59998a8b501f630e7614cd7d8b297d
DESKTOP-UG3PI5T\Novakovic <nikola2312017@gmail.com>
2022-01-11 20:49:31 +0100

    URADITI QUANTITY CIRCLE

--- script.js
+++ script.js
@@ -14,15 +14,23 @@ let productPrice = document.querySelector('.price-product')
 let productViewSection = document.querySelector('.product-view-section')
 let heroSectionProducts = document.querySelector('.hero-section-products')
 
+// cart
+let quantityInput = document.querySelector('.quantity-input')
+let quantityInputValue = null;
+let cartMargin = document.querySelector('.cart-margin')
+let buyBtn2 = document.querySelector('.buy-btn-2')
+
+let circleNum = document.querySelector('.circle-num')
+
+
 // MORAS DA KREIRAS PA DA SELEKTUJES !!!!!!!!!!!!!!!!!!!!!!!!!!
-let text = ""
 createProducts()
 
 // buttons
-let viewBtns = document.querySelectorAll('.view-btn')
-let buyBtns = document.querySelectorAll('.buy-btn')
+
 
 function createProducts() {
+    let text = ""
 
     for (let i = 0; i < db.length; i++) {
         text += `
@@ -40,18 +48,24 @@ function createProducts() {
         `
         heroSectionProducts.innerHTML = text
     }
+    let viewBtns = document.querySelectorAll('.view-btn')
+    let buyBtns = document.querySelectorAll('.buy-btn')
+
+        for (let i = 0; i < viewBtns.length; i++) {
+            viewBtns[i].addEventListener('click', viewProduct)
+            buyBtns[i].addEventListener('click', addToCart)
+        }
 }
 // MORAS DA KREIRAS PA DA SELEKTUJES !!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // addEvent
-for (let i = 0; i < viewBtns.length; i++) {
-    viewBtns[i].addEventListener('click', viewProduct)
-}
 
 let index = null
 
 function viewProduct() {
     index = this.getAttribute("data-index")
+    buyBtn2.setAttribute("data-index", index)
+    buyBtn2.addEventListener('click', addToCart)
 
     blockImg.innerHTML = `<img class="product-img" src="${db[index].imgUrl}" alt=""></img>`
     
@@ -62,35 +76,25 @@ function viewProduct() {
     heroSectionProducts.style.display = "none"
 }
 
-
-let productShow = ""
+let productShow = "" 
 // cart functionality
-let quantityInput = document.querySelector('.quantity-input')
-let qunatityValue = quantityInput.value
-let cartMargin = document.querySelector('.cart-margin')
-let buyBtn2 = document.querySelector('.buy-btn-2')
-
-
-buyBtn2.addEventListener('click', addToCart)
-
-for (let i = 0; i < buyBtns.length; i++) {
-    buyBtns[i].addEventListener('click', addToCart)
-}
-
 function addToCart() {
     index = this.getAttribute("data-index")
 
+    quantityInputValue = quantityInput.value
+    
     productShow += `
     <div class="cart-product">
             <img class="cart-shoe-img" src="${db[index].imgUrl}" alt="">
             <div class="cart-content-div">
                 <h3 class="cart-shoe-title">${db[index].shoeTitle}</h3>
                 <p class="cart-shoe-price">${db[index].shoePrice}$</p>
-                <h4 class="cart-shoe-quantity">Quantity: ${qunatityValue}</h4>
+                <h4 class="cart-shoe-quantity">Quantity: ${quantityInputValue}</h4>
+                <h4 class="cart-total-amount">${totalAmount} $</h4>
             </div>
     </div>
-
     `   
     cartMargin.innerHTML = productShow
-    
 }
+
+
